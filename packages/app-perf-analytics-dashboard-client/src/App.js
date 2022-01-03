8d3ea609613b61917fe6298fecb57b261c5d985a
cantanert <cantanert@gmail.com>
2022-01-03 13:35:24 +0300

    A sample project implementation (app-perf-analytics-data-provider-client) that for providing data to related API and Dashboard project implementation (app-perf-analytics-dashboard-client) for visualization the data that came from related API

---
+++ packages/app-perf-analytics-dashboard-client/src/App.js
@@ -0,0 +1,17 @@
+
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>PerfAnalytics Dashboard</h1>
+        <p>
+          Dashboard for performance analytics visualization
+        </p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
---
+++ packages/app-perf-analytics-data-provider-client/src/App.js
@@ -0,0 +1,16 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>DATA PROVIDER APP</h1>
+        <p>This app created for providing real data to related API</p>
+        <img src={logo} className="App-logo" alt="logo" />
+      </header>
+    </div>
+  );
+}
+
+export default App;
